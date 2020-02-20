02839a0d829f27a5b5d8578ab281fd32172ef129
Junaid Rahim <junaidrahim8d@gmail.com>
2020-02-20 23:48:10 +0530

    added input code

--- 2020/qualification_round/sol.cpp
+++ 2020/qualification_round/sol.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
 #include <sstream>
 
 
@@ -17,7 +19,41 @@ vector<string> split(const string& s, char delimiter) {
     return tokens;
 }
 
+vector<string> readFile(string filename){
+    vector<string> data;
+
+    ifstream fin;
+    fin.open(filename);
+
+    string line;
+    
+    while(fin){
+        getline(fin, line);
+        data.push_back(line);
+    }
+
+    return data;
+}
+
 int main(){
-    cout << "Hello hashcode" << endl;
+    string filename = "input_data/a_example.txt";
+
+    vector<string> file_data = readFile(filename);
+    vector<string> scores_string_arr = split(file_data[1], ' ');
+
+    int no_of_books, no_of_libs, days;
+
+    no_of_books = atoi(split(file_data[0], ' ')[0].c_str());
+    no_of_libs = atoi(split(file_data[0],' ')[1].c_str());
+    days = atoi(split(file_data[0], ' ')[2].c_str());
+
+    int* scores = new int[no_of_books];
+
+    for(int i=0; i<no_of_books; i++){
+        scores[i] = atoi(scores_string_arr[i].c_str());
+    }
+
+    
+
     return 0;
 }
