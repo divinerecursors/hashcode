a9c7a357c3338a76eaee7344812f41c75f74dc2e
Junaid Rahim <junaidrahim8d@gmail.com>
2020-02-20 22:05:31 +0530

    added split function

--- 2019/qualification_round/test.cpp
+++ 2019/qualification_round/test.cpp
@@ -1,28 +1,44 @@
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <sstream>
+
+// using namespace std;
+
+// vector<string> split(const string& s, char delimiter) {
+//    vector<string> tokens;
+//    string token;
+//    istringstream tokenStream(s);
+//    while (getline(tokenStream, token, delimiter))
+//    {
+//       tokens.push_back(token);
+//    }
+//    return tokens;
+// }
+
+// int main(){
+
+//     vector<string> f = split("My name is Junaid", ' ');
+//     f.erase(f.begin(), f.begin()+2);
+
+//     for(string k: f)
+//         cout << k << endl;
+
+//     return 0;
+// }
+
+
+////////////////////////////////////////////////////////
+
 #include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <tuple>
 
 using namespace std;
 
-vector<string> split(const string& s, char delimiter) {
-   vector<string> tokens;
-   string token;
-   istringstream tokenStream(s);
-   while (getline(tokenStream, token, delimiter))
-   {
-      tokens.push_back(token);
-   }
-   return tokens;
-}
-
 int main(){
+    tuple<int, int> k (1,2);
 
-    vector<string> f = split("My name is Junaid", ' ');
-    f.erase(f.begin(), f.begin()+2);
-
-    for(string k: f)
-        cout << k << endl;
+    cout << get<0>(k) << endl;
 
     return 0;
 }
--- 2020/qualification_round/sol.cpp
+++ 2020/qualification_round/sol.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+
 
 using namespace std;
+
+vector<string> split(const string& s, char delimiter) {
+    vector<string> tokens;
+    string token;
+    
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter)){
+        tokens.push_back(token);
+    }
+   
+    return tokens;
+}
 
 int main(){
     cout << "Hello hashcode" << endl;
